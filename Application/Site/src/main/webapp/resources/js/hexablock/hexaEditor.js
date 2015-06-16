86776c7695e5cd01777e8c0806991522ec4136c0
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-06-16 09:15:06 +0200

    Add model for bag and life

--- Application/Site/src/main/webapp/resources/js/hexablock/hexaEditor.js
+++ Application/Site/src/main/webapp/resources/js/hexablock/hexaEditor.js
@@ -143,225 +143,285 @@ var HexaEditorStream = Stream.extend({
                 case "actionWhen":
                     hexagon.sprite = actionWhen;
                     hexagon.typeOfType = 110;
-                    return hexagon;
                     break;
                 case "actionDo":
                     hexagon.sprite = actionDo;
                     hexagon.typeOfType = 120;
-                    return hexagon;
                     break;
                 case "operatorAnd" :
                     hexagon.sprite = operatorAnd;
                     hexagon.typeOfType = 131;
-                    return hexagon;
                     break;
                 case "operatorOr" :
                     hexagon.sprite = operatorOr;
                     hexagon.typeOfType = 132;
-                    return hexagon;
                     break;
                 case "operatorNot" :
                     hexagon.sprite = operatorNot;
                     hexagon.typeOfType = 133;
-                    return hexagon;
                     break;
                 case "agentBaseTeam1":
                     hexagon.sprite = agentBaseTeam1;
                     hexagon.typeOfType = 211;
-                    return hexagon;
                     break;
                 case "agentEngineerTeam1":
                     hexagon.sprite = agentEngineerTeam1;
                     hexagon.typeOfType = 212;
-                    return hexagon;
                     break;
                 case "agentExplorerTeam1":
                     hexagon.sprite = agentExplorerTeam1;
                     hexagon.typeOfType = 213;
-                    return hexagon;
                     break;
                 case "agentKamikazeTeam1":
                     hexagon.sprite = agentKamikazeTeam1;
                     hexagon.typeOfType = 214;
-                    return hexagon;
                     break;
                 case "agentRocketLauncherTeam1":
                     hexagon.sprite = agentRocketLauncherTeam1;
                     hexagon.typeOfType = 215;
-                    return hexagon;
                     break;
                 case "agentTurretTeam1":
                     hexagon.sprite = agentTurretTeam1;
                     hexagon.typeOfType = 216;
-                    return hexagon;
                     break;
                 case "agentWallTeam1":
                     hexagon.sprite = agentWallTeam1;
                     hexagon.typeOfType = 217;
-                    return hexagon;
                     break;
                 case "agentBaseTeam2":
                     hexagon.sprite = agentBaseTeam2;
                     hexagon.typeOfType = 221;
-                    return hexagon;
                     break;
                 case "agentEngineerTeam2":
                     hexagon.sprite = agentEngineerTeam2;
                     hexagon.typeOfType = 222;
-                    return hexagon;
                     break;
                 case "agentExplorerTeam2":
                     hexagon.sprite = agentExplorerTeam2;
                     hexagon.typeOfType = 223;
-                    return hexagon;
                     break;
                 case "agentKamikazeTeam2":
                     hexagon.sprite = agentKamikazeTeam2;
                     hexagon.typeOfType = 224;
-                    return hexagon;
                     break;
                 case "agentRocketLauncherTeam2":
                     hexagon.sprite = agentRocketLauncherTeam2;
                     hexagon.typeOfType = 225;
-                    return hexagon;
                     break;
                 case "agentTurretTeam2":
                     hexagon.sprite = agentTurretTeam2;
                     hexagon.typeOfType = 226;
-                    return hexagon;
                     break;
                 case "agentWallTeam2":
                     hexagon.sprite = agentWallTeam2;
                     hexagon.typeOfType = 227;
-                    return hexagon;
                     break;
                 case "agentFood":
                     hexagon.sprite = agentFood;
                     hexagon.typeOfType = 231;
-                    return hexagon;
                     break;
                 case "viewAgent":
                     hexagon.sprite = viewAgent;
                     hexagon.typeOfType = 301;
-                    return hexagon;
                     break;
                 case "viewBaseTeam1":
                     hexagon.sprite = viewBaseTeam1;
                     hexagon.typeOfType = 311;
-                    return hexagon;
                     break;
                 case "viewEngineerTeam1":
                     hexagon.sprite = viewEngineerTeam1;
                     hexagon.typeOfType = 312;
-                    return hexagon;
                     break;
                 case "viewExplorerTeam1":
                     hexagon.sprite = viewExplorerTeam1;
                     hexagon.typeOfType = 313;
-                    return hexagon;
                     break;
                 case "viewKamikazeTeam1":
                     hexagon.sprite = viewKamikazeTeam1;
                     hexagon.typeOfType = 314;
-                    return hexagon;
                     break;
                 case "viewRocketLauncherTeam1":
                     hexagon.sprite = viewRocketLauncherTeam1;
                     hexagon.typeOfType = 315;
-                    return hexagon;
                     break;
                 case "viewTurretTeam1":
                     hexagon.sprite = viewTurretTeam1;
                     hexagon.typeOfType = 316;
-                    return hexagon;
                     break;
                 case "viewWallTeam1":
                     hexagon.sprite = viewWallTeam1;
                     hexagon.typeOfType = 317;
-                    return hexagon;
                     break;
                 case "viewBaseTeam2":
                     hexagon.sprite = viewBaseTeam2;
                     hexagon.typeOfType = 321;
-                    return hexagon;
                     break;
                 case "viewEngineerTeam2":
                     hexagon.sprite = viewEngineerTeam2;
                     hexagon.typeOfType = 322;
-                    return hexagon;
                     break;
                 case "viewExplorerTeam2":
                     hexagon.sprite = viewExplorerTeam2;
                     hexagon.typeOfType = 323;
-                    return hexagon;
                     break;
                 case "viewKamikazeTeam2":
                     hexagon.sprite = viewKamikazeTeam2;
                     hexagon.typeOfType = 324;
-                    return hexagon;
                     break;
                 case "viewRocketLauncherTeam2":
                     hexagon.sprite = viewRocketLauncherTeam2;
                     hexagon.typeOfType = 325;
-                    return hexagon;
                     break;
                 case "viewTurretTeam2":
                     hexagon.sprite = viewTurretTeam2;
                     hexagon.typeOfType = 326;
-                    return hexagon;
                     break;
                 case "viewWallTeam2":
                     hexagon.sprite = viewWallTeam2;
                     hexagon.typeOfType = 327;
-                    return hexagon;
                     break;
                 case "viewFood":
                     hexagon.sprite = viewFood;
                     hexagon.typeOfType = 331;
-                    return hexagon;
                     break;
                 case "actionEat":
                     hexagon.sprite = actionEat;
                     hexagon.typeOfType = 410;
-                    return hexagon;
                     break;
                 case "actionGive":
                     hexagon.sprite = actionGive;
                     hexagon.typeOfType = 420;
-                    return hexagon;
                     break;
                 case "actionIdle":
                     hexagon.sprite = actionIdle;
                     hexagon.typeOfType = 430;
-                    return hexagon;
                     break;
                 case "actionMove":
                     hexagon.sprite = actionMove;
                     hexagon.typeOfType = 440;
-                    return hexagon;
                     break;
                 case "actionTake":
                     hexagon.sprite = actionTake;
                     hexagon.typeOfType = 450;
-                    return hexagon;
                     break;
                 case "actionFire":
                     hexagon.sprite = actionFire;
                     hexagon.typeOfType = 460;
-                    return hexagon;
                     break;
                 case "actionReload":
                     hexagon.sprite = actionReload;
                     hexagon.typeOfType = 470;
-                    return hexagon;
+                    break;
+                case "bag00" :
+                    hexagon.sprite = bag00;
+                    hexagon.typeOfType = 500;
+                    break;
+                case "bag10" :
+                    hexagon.sprite = bag10;
+                    hexagon.typeOfType = 501;
+                    break;
+                case "bag20" :
+                    hexagon.sprite = bag20;
+                    hexagon.typeOfType = 502;
+                    break;
+                case "bag25" :
+                    hexagon.sprite = bag25;
+                    hexagon.typeOfType = 502.5;
+                    break;
+                case "bag30" :
+                    hexagon.sprite = bag30;
+                    hexagon.typeOfType = 503;
+                    break;
+                case "bag40" :
+                    hexagon.sprite = bag40;
+                    hexagon.typeOfType = 504;
+                    break;
+                case "bag50" :
+                    hexagon.sprite = bag50;
+                    hexagon.typeOfType = 505;
+                    break;
+                case "bag60" :
+                    hexagon.sprite = bag60;
+                    hexagon.typeOfType = 506;
+                    break;
+                case "bag70" :
+                    hexagon.sprite = bag70;
+                    hexagon.typeOfType = 507;
+                    break;
+                case "bag75" :
+                    hexagon.sprite = bag75;
+                    hexagon.typeOfType = 507.5;
+                    break;
+                case "bag80" :
+                    hexagon.sprite = bag80;
+                    hexagon.typeOfType = 508;
+                    break;
+                case "bag90" :
+                    hexagon.sprite = bag90;
+                    hexagon.typeOfType = 509;
+                    break;
+                case "bag100" :
+                    hexagon.sprite = bag100;
+                    hexagon.typeOfType = 510;
+                    break;
+                case "life00" :
+                    hexagon.sprite = life00;
+                    hexagon.typeOfType = 600;
+                    break;
+                case "life10" :
+                    hexagon.sprite = life10;
+                    hexagon.typeOfType = 601;
+                    break;
+                case "life20" :
+                    hexagon.sprite = life20;
+                    hexagon.typeOfType = 602;
+                    break;
+                case "life25" :
+                    hexagon.sprite = life25;
+                    hexagon.typeOfType = 602.5;
+                    break;
+                case "life30" :
+                    hexagon.sprite = life30;
+                    hexagon.typeOfType = 603;
+                    break;
+                case "life40" :
+                    hexagon.sprite = life40;
+                    hexagon.typeOfType = 604;
+                    break;
+                case "life50" :
+                    hexagon.sprite = life50;
+                    hexagon.typeOfType = 605;
+                    break;
+                case "life60" :
+                    hexagon.sprite = life60;
+                    hexagon.typeOfType = 606;
+                    break;
+                case "life70" :
+                    hexagon.sprite = life70;
+                    hexagon.typeOfType = 607;
+                    break;
+                case "life75" :
+                    hexagon.sprite = life75;
+                    hexagon.typeOfType = 607.5;
+                    break;
+                case "life80" :
+                    hexagon.sprite = life80;
+                    hexagon.typeOfType = 608;
+                    break;
+                case "life90" :
+                    hexagon.sprite = life90;
+                    hexagon.typeOfType = 609;
+                    break;
+                case "life100" :
+                    hexagon.sprite = life100;
+                    hexagon.typeOfType = 610;
                     break;
                 default:
                     hexagon.sprite = emptyBlock;
                     hexagon.typeOfType = 0;
-                    return hexagon;
             }
         }
-        return null;
+        return hexagon;
     },
 
     createBlock : function(scene, form, cX, cY, type, indexTab) {
@@ -518,6 +578,14 @@ var HexaEditorStream = Stream.extend({
             else {
                 // TODO VERIF POUR NOT
             }
+
+            this.createBlock(this.camera, this.nextHexagonToCreate, block.position.x, block.position.y, 2, index);
+
+            tempBlock = this.getPositionOfNeighbourBlock(block, 1); // BLOCK EMPTY
+            this.createBlock(this.camera, emptyBlock, tempBlock.x, tempBlock.y, 0.5, index);
+        }
+        else if (type >= 500 && type <= 610) { // BLOCK BAG + LIFE
+            // TODO VERIF EMPTY NO ECRASE
 
             this.createBlock(this.camera, this.nextHexagonToCreate, block.position.x, block.position.y, 2, index);
 
