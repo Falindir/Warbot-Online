1a6cfad3c1b6c25adb8b495857b0d0356676abec
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-08-05 21:16:45 +0200

    Update Editor Block 2

--- Application/Site/src/main/webapp/resources/js/hexaEditor/hexaEditorTexture.js
+++ Application/Site/src/main/webapp/resources/js/hexaEditor/hexaEditorTexture.js
@@ -6,11 +6,12 @@ blocksRegularSpriteSheet.cut();
 var blocksTruncatedSpriteSheet = new SpriteSheet('/resources/hexaBlocks/truncated.png', 2750, 4000, 250, 250);
 blocksTruncatedSpriteSheet.cut();
 
+var buttonHUDSpriteSheet = new SpriteSheet('/resources/hexaBlocks/buttonHUD.png', 1000, 600, 200, 200);
+buttonHUDSpriteSheet.cut();
+
 var blocksRegularTexture = new MapCollections();
 
 
+
+
 //var blocksTruncatedTexture = new MapCollections();
-
-
-
-
